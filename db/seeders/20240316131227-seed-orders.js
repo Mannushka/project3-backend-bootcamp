14bf47430884d046cefeabf5113ca5d614d181fd
ianthehamster <ianchow9898@gmail.com>
2024-03-16 22:34:27 +0800

    Seeded orders

---
+++ db/seeders/20240316131227-seed-orders.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('orders', [
+      {
+        quantity: 1,
+        delivery_address: 'Plaza Singapura',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        quantity: 3,
+        delivery_address: 'Orchard Road',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('orders', null, {});
+  },
+};
