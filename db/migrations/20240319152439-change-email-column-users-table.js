e21b44b3d9d4323ced57d121301ebe2675c88499
Mannushka <zaimar1910@gmail.com>
2024-03-19 23:32:25 +0800

    change email column in the users table

---
+++ db/migrations/20240319152439-change-email-column-users-table.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn("users", "email", {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.changeColumn("users", "email", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+};
